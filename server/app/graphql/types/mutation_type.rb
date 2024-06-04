0b5aa0d86a400559ec1a43eb5019df8dcd2a4e3e
corey <corey.cushnie@gmail.com>
2024-06-04 12:54:59 -0400

    mutation update

---
+++ server/app/graphql/types/mutation_type.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Types
+  class MutationType < Types::BaseObject
+    field :delete_list, mutation: Mutations::DeleteList
+    field :delete_task, mutation: Mutations::DeleteTask
+    field :update_task, mutation: Mutations::UpdateTask
+    field :create_task, mutation: Mutations::CreateTask
+
+    # TODO: remove me
+    field :test_field, String, null: false,
+      description: "An example field added by the generator"
+    def test_field
+      "Hello World"
+    end
+  end
+end
