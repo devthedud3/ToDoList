e141725b149052c6322ff8bc94784a913bed4e0d
corey <corey.cushnie@gmail.com>
2024-06-04 12:58:38 -0400

    routes

---
+++ server/config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: '/graphql', graphql_path: 'graphql#execute'
+  end
+
+  post "/graphql", to: "graphql#execute"
+
+  get "up" => "rails/health#show", as: :rails_health_check
+
+end
