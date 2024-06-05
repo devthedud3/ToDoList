385002a62615c18bcb31410a0f9c1a56f5f473b9
corey <corey.cushnie@gmail.com>
2024-06-05 16:03:43 -0400

    build page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <ApolloProvider client={client}>
       <main className="flex min-h-screen flex-col items-center p-24">
-        <h1 className="text-3xl mb-10">A Savvy Labs Task Manager</h1>
+        {/* <h1 className="text-3xl mb-10">A Savvy Labs Task Manager</h1> */}
         <List />
       </main>
     </ApolloProvider>
