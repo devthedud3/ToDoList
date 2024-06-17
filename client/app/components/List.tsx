63eb1feac4529a1c8ee273a971d01530b13bb877
corey <corey.cushnie@gmail.com>
2024-06-17 10:34:06 -0400

    list update

--- client/app/components/List.tsx
+++ client/app/components/List.tsx
@@ -1,22 +1,37 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useQuery } from "@apollo/client";
 import { GET_LIST } from "@/graphql/queries";
-import { CREATE_TASK, DELETE_LIST } from "@/graphql/mutations";
+import { DELETE_LIST } from "@/graphql/mutations";
 import { Task } from "./Task";
 import { Button } from "./Button";
 import { AddTask } from "./AddTask";
 
 export const List = () => {
+  const [list, setList] = useState([]);
   const { data, error, loading } = useQuery(GET_LIST);
+
+  useEffect(() => {
+    const cached = localStorage.getItem("lists");
+    if (cached) {
+      setList(JSON.parse(cached));
+    }
+  }, []);
+
+  useEffect(() => {
+    if (data && list.length !== 0) {
+      console.log("here");
+      const { lists } = data;
+      localStorage.setItem("lists", JSON.stringify(lists));
+      setList(lists);
+    }
+  }, [data, error, loading]);
 
   if (loading) return <p>Loading..</p>;
   if (error) return <p>Error loading tasks, try refreshing the page</p>;
 
-  const { lists } = data;
-
   return (
     <div className="w-full flex flex-wrap justify-center">
-      {lists.map((list: any, index: number) => (
+      {list.map((list: any, index: number) => (
         <div
           className="w-[512px] px-6 h-fit m-5 rounded bg-stone-100 py-10 border space-y-6"
           key={`${list}-${index}`}
@@ -30,7 +45,7 @@ export const List = () => {
             />
           </div>
           {list.tasks?.map((task: any) => (
-            <Task key={task.description} {...task} />
+            <Task key={`${task.id}${task.description}`} {...task} />
           ))}
           <AddTask list_id={`${list.id}`} />
         </div>
